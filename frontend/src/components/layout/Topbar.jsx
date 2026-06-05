e4ffca95b57a639c9a5fe24a26f5eddc30fdcc64
Nnavodya <rajapakse22030.kln@gmail.com>
2026-06-05 03:14:05 -0600

    feat: restructure Topbar component layout and add icon

--- frontend/src/components/layout/Topbar.jsx
+++ frontend/src/components/layout/Topbar.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { TbBrandMeta } from 'react-icons/tb';
+
 
 const Topbar = () => {
   return (
     <div className="bg-[#ea20e] text-white">
-      
+     <div className="container mx-auto ">
+      <a href="#" className="hover:text-gray-300">
+        <TbBrandMeta className= "h-5 w-5" />
+      </a>
+       <h1 className="text-2xl font-bold">FurniHub</h1>
+     </div>
     </div>
   )
 }
 
-export default Topbar
+export default Topbar;
