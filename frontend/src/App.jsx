39a5e3106c23fadefae4bcd9aafc1f3ae23f8ef9
Nnavodya <rajapakse22030.kln@gmail.com>
2026-06-05 09:22:41 +0530

    feat: update App component to display a welcome message and remove unused imports for testing purpose

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+/*import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
@@ -119,4 +119,16 @@ function App() {
   )
 }
 
-export default App
+export default App;*/
+
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-900 flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-white">
+        FurniHub 🪑
+      </h1>
+    </div>
+  );
+}
+
+export default App;
